f4b00019c2f906ca90c1a52b772b7469ba2e0ca1
Sambit Mishra <21ec01021@iitbbs.ac.in>
2023-12-03 12:12:34 +0530

    chat contacts done

---
+++ src/components/Body/Chats/IconChatButton.tsx
@@ -0,0 +1,41 @@
+import Image, { StaticImageData } from "next/image";
+import styles from "./IconChatButton.module.css";
+import { FC } from "react";
+
+interface Props {
+  img: StaticImageData;
+  imgAlt: string;
+  name: string;
+  lastChat: string;
+  selected: boolean;
+  onClick: () => void;
+}
+
+const IconChatButton: FC<Props> = ({ img, imgAlt, name, lastChat, selected, onClick }) => {
+  const showableLength = lastChat.length > 20 ? 20 : lastChat.length;
+  const contChat = showableLength < lastChat.length ? "..." : "";
+
+  return (
+    <div className={`${styles.container} ${selected && styles.selected}`} onClick={onClick}>
+      <div className={styles.imageContainer}>
+        <Image
+          src={img}
+          alt={imgAlt}
+          style={{
+            width: "100%",
+            height: "100%",
+            borderRadius: "50%",
+          }}
+        />
+      </div>
+      <div className={styles.chatContainer}>
+        <div className={styles.chatName}>{name}</div>
+        <div className={styles.lastChat}>
+          {lastChat.substring(0, showableLength) + contChat}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IconChatButton;
